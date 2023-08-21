7eaf083f6de05dd419a42c4795e565eddf6aba7d
AtilaRocha <atila.o.rocha@gmail.com>
2023-08-21 19:49:14 -0300

    Projeto gomes-pizzaria created

---
+++ gomes-pizzaria/src/test/java/com/gomes/pizzaria/GomesPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomes.pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomesPizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
